a89dfa93cb491a28a4b5d8956107289b05b7b6ea
HinmQ21 <120171053+HinmQ21@users.noreply.github.com>
2023-09-27 09:09:43 +0700

    change_ver1

--- Dictionary_ver1/src/DictionaryCommandLine.java
+++ Dictionary_ver1/src/DictionaryCommandLine.java
@@ -1,37 +1,31 @@
 import java.util.List;
 
 public class DictionaryCommandLine {
-    Dictionary dictionary = new Dictionary();
-    DictionaryManagement dictionaryManagement = new DictionaryManagement();
+    private Dictionary dictionary;
+    private DictionaryManagement dictionaryManagement;
 
-    public void showAllWords() {
-        System.out.println("No | English        | Vietnamese");
-        List<Word> words = dictionary.getWords();
-        for (int i = 0; i < words.size(); i++) {
-            Word word = words.get(i);
-            System.out.printf("%-3d| %-15s| %s%n", (i + 1), word.getWordTarget(), word.getWordExplain());
-        }
+    public DictionaryCommandLine() {
+        dictionary = new Dictionary();
+        dictionaryManagement = new DictionaryManagement(dictionary);
     }
-
-    public void dictionaryBasic() {
-        dictionaryManagement.insertFromCommandLine(dictionary);
-        showAllWords();
+    public void showMenu() {
+        System.out.println("Welcome to My Application!");
+        System.out.println("[0] Exit");
+        System.out.println("[1] Add");
+        System.out.println("[2] Remove");
+        System.out.println("[3] Update");
+        System.out.println("[4] Display");
+        System.out.println("[5] Lookup");
+        System.out.println("[6] Search");
+        System.out.println("[7] Game");
+        System.out.println("[8] Import from file");
+        System.out.println("[9] Export to file");
+        System.out.print("Your action: ");
     }
 
     public void dictionaryAdvanced() {
         while (true) {
-            System.out.println("Welcome to My Application!");
-            System.out.println("[0] Exit");
-            System.out.println("[1] Add");
-            System.out.println("[2] Remove");
-            System.out.println("[3] Update");
-            System.out.println("[4] Display");
-            System.out.println("[5] Lookup");
-            System.out.println("[6] Search");
-            System.out.println("[7] Game");
-            System.out.println("[8] Import from file");
-            System.out.println("[9] Export to file");
-            System.out.print("Your action: ");
+            showMenu();
 
             int choice = dictionaryManagement.scanner.nextInt();
             dictionaryManagement.scanner.nextLine(); // Consume the newline character
@@ -40,61 +34,28 @@ public class DictionaryCommandLine {
                 case 0:
                     return;
                 case 1:
-                    System.out.print("Enter word in English: ");
-                    String wordTarget = dictionaryManagement.scanner.nextLine();
-                    System.out.print("Enter word in Vietnamese: ");
-                    String wordExplain = dictionaryManagement.scanner.nextLine();
-                    Word word = new Word(wordTarget, wordExplain);
-                    dictionary.insertWord(word);
+                    dictionaryManagement.addWord();
                     break;
                 case 2:
-                    System.out.print("Enter the word to remove: ");
-                    wordTarget = dictionaryManagement.scanner.nextLine();
-                    Word removeWord = dictionary.lookupWord(wordTarget);
-                    if (removeWord != null) {
-                        dictionary.removeWord(removeWord);
-                    } else {
-                        System.out.println("Word not found in the dictionary.");
-                    }
+                    dictionaryManagement.removeWord();
                     break;
                 case 3:
-                    System.out.print("Enter the word to update: ");
-                    String oldWordTarget = dictionaryManagement.scanner.nextLine();
-                    Word oldWord = dictionary.lookupWord(oldWordTarget);
-                    if (oldWord != null) {
-                        System.out.print("Enter the new word in English: ");
-                        String newWordTarget = dictionaryManagement.scanner.nextLine();
-                        System.out.print("Enter the new word in Vietnamese: ");
-                        String newWordExplain = dictionaryManagement.scanner.nextLine();
-                        Word newWord = new Word(newWordTarget, newWordExplain);
-                        dictionary.updateWord(oldWord, newWord);
-                    } else {
-                        System.out.println("Word not found in the dictionary.");
-                    }
+                    dictionaryManagement.updateWord();
                     break;
                 case 4:
-                    showAllWords();
+                    dictionaryManagement.showAllWords();
                     break;
                 case 5:
                     dictionaryManagement.dictionaryLookup(dictionary);
                     break;
                 case 6:
-                    System.out.print("Enter the prefix to search: ");
-                    String prefix = dictionaryManagement.scanner.nextLine();
-                    List<String> result = dictionary.searchWords(prefix);
-                    System.out.println("Words found:");
-                    for (String wordResult : result) {
-                        System.out.println(wordResult);
-                    }
+                    dictionaryManagement.searchWord();
                     break;
                 case 7:
                     System.out.println("Game feature is not supported yet.");
                     break;
                 case 8:
-                    //System.out.print("Enter the filename to import: ");
-                    //String importFilename = dictionaryManagement.scanner.nextLine();
-                    String importFilename = "src\\dictionaries.txt";
-                    dictionary.importFromFile(importFilename);
+                    dictionaryManagement.dictionaryImportFromFile();
                     break;
                 case 9:
                     dictionaryManagement.dictionaryExportToFile(dictionary);
--- Dictionary_ver1/src/DictionaryManagement.java
+++ Dictionary_ver1/src/DictionaryManagement.java
@@ -1,19 +1,82 @@
+import java.util.List;
 import java.util.Scanner;
 
 public class DictionaryManagement {
     Scanner scanner = new Scanner(System.in);
 
-    public void insertFromCommandLine(Dictionary dictionary) {
-        System.out.print("Enter the number of words: ");
-        int n = scanner.nextInt();
-        scanner.nextLine(); // Consume the newline character
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter word in English: ");
-            String wordTarget = scanner.nextLine();
-            System.out.print("Enter word in Vietnamese: ");
-            String wordExplain = scanner.nextLine();
-            Word word = new Word(wordTarget, wordExplain);
-            dictionary.insertWord(word);
+    Dictionary dictionary ;
+    public DictionaryManagement(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+//    public void insertFromCommandLine(Dictionary dictionary) {
+//        System.out.print("Enter the number of words: ");
+//        int n = scanner.nextInt();
+//        scanner.nextLine(); // Consume the newline character
+//        for (int i = 0; i < n; i++) {
+//            System.out.print("Enter word in English: ");
+//            String wordTarget = scanner.nextLine();
+//            System.out.print("Enter word in Vietnamese: ");
+//            String wordExplain = scanner.nextLine();
+//            Word word = new Word(wordTarget, wordExplain);
+//            dictionary.insertWord(word);
+//        }
+//    }
+
+    public void addWord() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the English word: ");
+        String wordTarget = scanner.nextLine();
+        System.out.print("Enter the Vietnamese explanation: ");
+        String wordExplain = scanner.nextLine();
+        Word word = new Word(wordTarget, wordExplain);
+        dictionary.insertWord(word);
+    }
+
+    public void removeWord() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the English word to remove: ");
+        String wordTarget = scanner.nextLine();
+        Word word = dictionary.lookupWord(wordTarget);
+        if (word != null) {
+            dictionary.removeWord(word);
+            System.out.println("Word removed successfully.");
+        } else {
+            System.out.println("Word not found in the dictionary.");
+        }
+    }
+
+    public void updateWord() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the English word to update: ");
+        String wordTarget = scanner.nextLine();
+        Word word = dictionary.lookupWord(wordTarget);
+        if (word != null) {
+            System.out.print("Enter the new Vietnamese explanation: ");
+            String newExplain = scanner.nextLine();
+            Word new_word = new Word(wordTarget, newExplain);
+            dictionary.updateWord(word, new_word);
+            System.out.println("Word updated successfully.");
+        } else {
+            System.out.println("Word not found in the dictionary.");
+        }
+    }
+
+    public void searchWord() {
+        System.out.print("Enter the prefix to search: ");
+        String prefix = scanner.nextLine();
+        List<String> result = dictionary.searchWords(prefix);
+        System.out.println("Words found:");
+        for (String wordResult : result) {
+            System.out.println(wordResult);
+        }
+    }
+    public void showAllWords() {
+        System.out.println("No | English        | Vietnamese");
+        List<Word> words = dictionary.getWords();
+        for (int i = 0; i < words.size(); i++) {
+            Word word = words.get(i);
+            System.out.printf("%-3d| %-15s| %s%n", (i + 1), word.getWordTarget(), word.getWordExplain());
         }
     }
 
@@ -27,6 +90,13 @@ public class DictionaryManagement {
         } else {
             System.out.println("Word not found in the dictionary.");
         }
+    }
+
+    public void dictionaryImportFromFile() {
+//        System.out.print("Enter the filename to import: ");
+//        String importFile = scanner.nextLine();
+        String importFilename = "src\\dictionaries.txt";
+        dictionary.importFromFile(importFilename);
     }
 
     public void dictionaryExportToFile(Dictionary dictionary) {
