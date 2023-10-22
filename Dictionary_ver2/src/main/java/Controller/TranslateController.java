0992cab8b37ec4d2c3ab633bd12f14f21a7368c2
HinmQ21 <120171053+HinmQ21@users.noreply.github.com>
2023-10-22 22:20:17 +0700

    update_translate

--- Dictionary_ver2/src/main/java/Controller/TranslateController.java
+++ Dictionary_ver2/src/main/java/Controller/TranslateController.java
@@ -3,17 +3,52 @@ package Controller;
 import DictionaryComandLine.DictionaryManagement;
 import javafx.fxml.FXML;
 
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class TranslateController {
+public class TranslateController implements Initializable {
+
     @FXML
-    TextArea vnText, enText;
+    TextArea rightText, leftText;
+
+    @FXML
+    Label rightLabel, leftLabel;
+
+    private String langFrom, langTo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        rightLabel.setText("VIETNAMESE");
+        leftLabel.setText("ENGLISH");
+        langFrom = "en";
+        langTo = "vi";
+    }
 
     public void onActionTran() throws IOException {
-        String vnInput = vnText.getText();
-        String res = DictionaryManagement.translate("en", "vi", vnInput);
-        enText.setText(res);
+        String vnInput = leftText.getText();
+        String res = DictionaryManagement.translate(langFrom, langTo, vnInput);
+        rightText.setText(res);
     }
+
+    public void onActionSwitch() {
+        if (langFrom.equals("vi")) {
+            langFrom = "en";
+            langTo = "vi";
+            leftLabel.setText("ENGLISH");
+            rightLabel.setText("VIETNAMESE");
+        } else {
+            langFrom = "vi";
+            langTo = "en";
+            leftLabel.setText("VIETNAMESE");
+            rightLabel.setText("ENGLISH");
+        }
+        leftText.clear();
+        rightText.clear();
+    }
+
 }
