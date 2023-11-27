24cf391152f3d51b378f85df20674dc07ccab577
HinmQ21 <120171053+HinmQ21@users.noreply.github.com>
2023-11-27 18:52:37 +0700

    suggest

---
+++ Dictionary_ver2/src/main/java/Controller/Game_Controller/LoseHangManGame.java
@@ -0,0 +1,26 @@
+package Controller.Game_Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+
+public class LoseHangManGame {
+    private GameHangMan gameHangMan = new GameHangMan();
+    @FXML
+    AnchorPane hangManContainer;
+    public void onActionPlayAgain() {
+        try {
+            BorderPane component = FXMLLoader.load(getClass().getResource("/fxml/MenuHangManGame.fxml"));
+            hangManContainer.getChildren().clear();
+            hangManContainer.getChildren().add(component);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gameHangMan.getWords().clear();
+    }
+}
